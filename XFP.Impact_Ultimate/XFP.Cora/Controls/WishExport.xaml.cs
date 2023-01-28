a14003c1206caf3c99b6c85a2c45be2a4976075a
x3zFLoveU <120739043+GashByte@users.noreply.github.com>
2023-01-28 18:07:22 +0800

    🔥Add Wish Export Page!

---
+++ XFP.Impact_Ultimate/XFP.Cora/Controls/WishExport.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace XFP.ICora.Controls
+{
+    /// <summary>
+    /// WishExport.xaml 的交互逻辑
+    /// </summary>
+    public partial class WishExport : UserControl
+    {
+        public WishExport()
+        {
+            InitializeComponent();
+        }
+    }
+}
